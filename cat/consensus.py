7743393464f334decf3b71269e0f1facf962a7a1
Prajna Hebbar <hebbarprajna2000@gmail.com>
2023-07-08 21:28:20 -0700

    Update consensus.py

--- cat/consensus.py
+++ cat/consensus.py
@@ -586,6 +586,7 @@ def find_novel(db_path, tx_dict, consensus_dict, ref_df, metrics, gene_biotype_m
     def is_novel_supported(s):
         """Is this CGP/PB transcript with an assigned gene ID supported and have a novel splice?"""
         denovo_tx_obj = tx_dict[s.AlignmentId]
+        print(denovo_tx_obj.intron_intervals)
         if len(denovo_tx_obj.intron_intervals) < denovo_num_introns:
             return None
         elif in_species_rna_support_only and s.ExonRnaSupportPercent <= denovo_exon_support or \
