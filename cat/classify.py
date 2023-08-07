97e14b05b496adbe94d2251d323777a55ec86020
Prajna Hebbar <hebbarprajna2000@gmail.com>
2023-08-07 12:56:13 -0700

    Update classify.py

--- cat/classify.py
+++ cat/classify.py
@@ -322,8 +322,6 @@ def tx_iter(psl_iter, ref_tx_dict, tx_dict, tx_biotype_map):
     for psl in psl_iter:
         # this psl is target-referenced
         ref_tx = ref_tx_dict[psl.t_name]
-        print(psl.t_name)
-        print(psl.q_name)
         tx = tx_dict[psl.q_name]
         biotype = tx_biotype_map[psl.t_name]
         yield ref_tx, tx, psl, biotype
